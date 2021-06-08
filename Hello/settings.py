24dd411e8a682cd1f13cc73e9691d8fec19b1e4a
Yathin Prakash Kethepalli <yathinprakash017@gmail.com>
2021-06-09 00:31:40 +0530

    commit

--- Hello/settings.py
+++ Hello/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-f@#lmtvm@)b5q*%qfypzh(iuybe2o$%lh=u!nx#$h$z1pfvb%$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','student-report-portal-django.herokuapp.com']
 
 
 # Application definition
